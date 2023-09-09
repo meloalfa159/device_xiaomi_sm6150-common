5396517c8e63d476a5ad320ce3b6ee57d9c2c0c8
meloalfa159 <melolongo87@gmail.com>
2023-09-09 21:13:17 +0200

    sm6150-common: adapt to EvolutionX

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,7 +66,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/derp/config/device_framework_matrix.xml \
+    vendor/evolution/config/device_framework_matrix.xml \
     $(COMMON_PATH)/configs/hidl/audio_dolby.xml
 
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
