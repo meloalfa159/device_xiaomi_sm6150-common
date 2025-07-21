85dc0c133d48185e98be773e87b4d375113e8b32
meloalfa159 <melolongo87@gmail.com>
2025-07-21 18:54:04 +0200

    sm6150-common: Infinity-X bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,7 +54,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/infinity/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
